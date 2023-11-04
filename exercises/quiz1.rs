0ba3f49d477c4c086d3255c68d098e239464624e
devlkx <liukaixiangid@gmail.com>
2023-11-04 19:34:07 +0800

    day1 quiz1 done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(i: u32) -> u32 {
+    if i > 40 {
+        return i;
+    }
+    i * 2
+}
 
 // Don't modify this function!
 #[test]
